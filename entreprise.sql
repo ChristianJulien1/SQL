13517d105cdab1d57bfc15749a27200175987c14
ChristianJulien1 <m.c.j.2105@outlook.fr>
2023-05-15 16:09:26 +0200

    sql

---
+++ entreprise.sql
@@ -0,0 +1,18 @@
+DROP DATABASE IF EXISTS entreprise;
+CREATE DATABASE IF NOT EXISTS entreprise;
+USE entreprise;
+
+# ----------------
+# TABLE : EMPLOYE
+# ----------------
+
+CREATE TABLE EMPLOYE (
+    idEmploye Int Auto_increment NOT NULL,
+    nom Varchar (50) NULL,
+    prenom Varchar (25) NULL,
+    sexe Char (1) NULL,
+    service Varchar (25) NULL,
+    salaire Int NULL,
+    dateEmbauche Date,
+    CONSTRAINT EMPLOYE-PK PRIMARY KEY (idEmploye),
+)ENGINE=InnoDB;
